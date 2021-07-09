d3f7e5bb6f59ad74331523b6149eb2a273c7af61
lima-wendel <wchristofer@gmail.com>
2021-07-08 22:05:11 -0300

    adicionando renderização condicional para usuário não encontrado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,6 +7,7 @@ import { useHistory } from 'react-router-dom';
 function App(props) {
   const history = useHistory();
   const [ usuario, setUsuario ] = useState('');
+  const [ erro, setErro ] = useState(false);
   
   function handlePesquisa(){
     axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => { 
@@ -16,14 +17,21 @@ function App(props) {
         repositoriesName.push(repository.name);
       });
       localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+      setErro(false);
       history.push('/repositories')
+  })
+  .catch(err => {
+    setErro(true);
   });
 }
   return (
-    <S.Container>
-      <S.Input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={ e => setUsuario(e.target.value)}/>
-      <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
-    </S.Container>
+    <S.HomeContainer>
+      <S.Content>
+        <S.Input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={ e => setUsuario(e.target.value)}/>
+        <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+      </S.Content>
+      { erro ? <S.ErrorMsg>Usuário não encontrado</S.ErrorMsg> : '' }
+    </S.HomeContainer>
   );
 }
 
